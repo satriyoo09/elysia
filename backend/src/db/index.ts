7c79c3ad1f7d4977c875691a2d99b40a5a43c492
ashoza <aufa8211@gmail.com>
2026-05-03 19:05:22 +0700

    Yeah idk

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -2,5 +2,12 @@ import { neon } from '@neondatabase/serverless'
 import { drizzle } from 'drizzle-orm/neon-http'
 import * as schema from './schema'
 
-const sql = neon(process.env.DATABASE_URL!)
+
+const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set")
+}
+
+const sql = neon(connectionString)
 export const db = drizzle(sql, { schema })
