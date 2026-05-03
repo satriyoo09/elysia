7044cc62b34d23f50cb1f32689485d340ee6474f
ashoza <aufa8211@gmail.com>
2026-05-03 21:26:22 +0700

    feat: initialize backend project with Elysia, Swagger, and Drizzle ORM configuration

--- backend/drizzle.config.ts
+++ backend/drizzle.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'drizzle-kit'
 import * as dotenv from 'dotenv'
 
+dotenv.config()
+
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema/index.ts',
   out: './drizzle',
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_TiNPAE9G5eWF@ep-dark-pond-aom09bmj.c-2.ap-southeast-1.aws.neon.tech/Elysia_db?sslmode=require&channel_binding=require",
+    url: process.env.DATABASE_URL!,
   },
 })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
 import { authRoute } from "./modules/auth/auth.route";
+import { swagger } from "@elysiajs/swagger";
+import { cors } from "@elysiajs/cors";
 
 const app = new Elysia()
+  .use(cors())
+  .use(swagger({
+    documentation: {
+      info: {
+        title: 'Elysia API Documentation',
+        version: '1.0.0'
+      }
+    }
+  }))
   .get("/", () => "Hello Elysia")
   .use(authRoute)
   .listen(3000);
